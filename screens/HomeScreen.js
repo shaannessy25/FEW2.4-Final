792922006170ca98031f96a35f03b447f3bf3813
shaannessy25 <shaan.hurley25@gmail.com>
2020-12-09 01:38:49 -0800

    flatlist displays band name

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet, } from 'react-native'
-
+import { SafeAreaView, View, FlatList, Text, StyleSheet, TouchableHighlight} from 'react-native'
+import Data from '../metal.json'
 
 export default function Home() {
+
+    const data = Data.map((item, index) => {
+        return {
+            name: item.band_name,
+            origin: item.origin,
+            // fans: item.fans,
+            // formed: item.formed
+        }
+    })
+    const Item = ({ name, origin }) => (
+        <View >
+          <Text>{name}</Text>
+          <Text>{origin}</Text>
+        </View>
+      );
+    
+      const renderItem = ({ item }) => (
+        <TouchableHighlight
+          activeOpacity={0.6}
+          underlayColor="#DDDDD"
+          onPress={() => alert('This is working')}
+        >
+        <Item name={item.name} />
+        </TouchableHighlight>
+      );
     return(
         <SafeAreaView>
-            <Text>Home Screen Works</Text>
+            <FlatList
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+                style={styles.listContainer}
+            />
         </SafeAreaView>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "column",
+      alignItems: "center",
+      width: "100%",
+    },
+    listContainer: {
+      width: "100%",
+    },
+  });
